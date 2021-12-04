8e14a66fb8859713056e27259017e8613bdf82e8
MisugiHaruo <misugiharuo@gmail.com>
2021-12-04 18:04:57 -0300

    Create desenvolvedores factory

---
+++ api/app/Constants/GenderConstants.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Constants;
+
+abstract class GenderConstants
+{
+    public const Female = 'F';
+    public const Male = 'M';
+    public const Other = 'O';
+}
---
+++ api/database/factories/DesenvolvedorFactory.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Constants\GenderConstants;
+use App\Models\Nivel;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DesenvolvedorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $genderSignList = [GenderConstants::Female, GenderConstants::Male, GenderConstants::Other];
+
+        $genderSign = $this->faker->randomElement($genderSignList);
+
+        $gender = match ($genderSign) {
+            GenderConstants::Female => 'female',
+            GenderConstants::Male => 'male',
+            GenderConstants::Other => null
+        };
+
+        $name = $this->faker->firstName($gender) . ' ' . $this->faker->lastName();
+
+        $birthDateTime = Carbon::parse($this->faker->dateTimeBetween('-30 years', '-22 years'));
+
+        return [
+            'nivel' => Nivel::inRandomOrder()->first(),
+            'nome' => $name,
+            'sexo' => $genderSign,
+            'datanascimento' => $birthDateTime->format('Y-m-d'),
+            'idade' => $birthDateTime->diffInYears(Carbon::now()),
+            'hobby' => $this->faker->randomElement($this->getHobbiesList())
+        ];
+    }
+
+    private function getHobbiesList(): array
+    {
+        return [
+            'Pescar',
+            'Tocar violão',
+            'Viajar',
+            'Natação',
+            'Dançar'
+        ];
+    }
+}

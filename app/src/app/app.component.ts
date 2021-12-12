11a0c27986aa0d5e18bc35b1d47ef9c17b14c856
MisugiHaruo <misugiharuo@gmail.com>
2021-12-12 10:42:48 -0300

    Include translation in the project

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { PrimeNGConfig } from 'primeng/api';
 import { LoadingService } from './services/loading.service';
 import { SideBarService } from './services/side-bar.service';
 
@@ -13,21 +15,36 @@ export class AppComponent implements OnInit
 
   constructor(
     private loadingService: LoadingService,
-    private sideBarService: SideBarService
+    private sideBarService: SideBarService,
+    private config: PrimeNGConfig,
+    private translateService: TranslateService
   )
   {}
 
   ngOnInit(): void
+  {
+    this.loadLoadingComponentVisibility();
+    this.configTranslation();
+  }
+
+  hideSideBar(): void
+  {
+    this.sideBarService.setShowSidebar(false);
+  }
+
+  private configTranslation(): void
+  {
+    this.translateService.setDefaultLang('en');
+    this.translateService.use('pt_BR');
+    this.translateService.get('primeng').subscribe(res => { this.config.setTranslation(res); });
+  }
+
+  private loadLoadingComponentVisibility(): void
   {
     this.loadingService.getShowLoading().subscribe(
       (showLoading: boolean) => {
         this.showLoadingComponent = showLoading;
       }
     );
-  }
-
-  hideSideBar(): void
-  {
-    this.sideBarService.setShowSidebar(false);
   }
 }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,14 @@ import { LoadingService } from './services/loading.service';
 import { SideBarService } from './services/side-bar.service';
 import { BrDatePipe } from './pipes/br-date.pipe';
 import { RouterModule } from '@angular/router';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { DesenvolvedorService } from './services/desenvolvedor.service';
+import { NivelService } from './services/nivel.service';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -20,15 +29,26 @@ import { RouterModule } from '@angular/router';
   imports: [
     AppRoutingModule,
     BrowserModule,
+    BrowserAnimationsModule,
     ComponentsModule,
     HttpClientModule,
-    RouterModule
+    RouterModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [
     LoadingService,
     NotificationService,
     RequestService,
-    SideBarService
+    SideBarService,
+    DesenvolvedorService,
+    NivelService
   ],
   bootstrap: [AppComponent]
 })

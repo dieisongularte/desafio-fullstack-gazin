40793d9543fa52d14f7e10eb3efb8cd904c4db21
MisugiHaruo <misugiharuo@gmail.com>
2021-12-13 02:28:23 -0300

    Improve nivel form component

--- app/src/app/views/nivel/form/nivel-form.component.ts
+++ app/src/app/views/nivel/form/nivel-form.component.ts
@@ -1,9 +1,10 @@
 import { NotificationService } from './../../../services/notification.service';
 import { Nivel } from './../../../entities/nivel';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NivelService } from 'src/app/services/nivel.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-nivel-form',
@@ -18,7 +19,8 @@ export class NivelFormComponent
     private activatedRoute: ActivatedRoute,
     private formBuilder: FormBuilder,
     private nivelService: NivelService,
-    private notificationService: NotificationService
+    private notificationService: NotificationService,
+    private router: Router
   )
   {
     this.buildForm();
@@ -27,11 +29,19 @@ export class NivelFormComponent
 
   async save(nivel: Nivel): Promise<void>
   {
-    this.nivelService
-      .save(nivel)
-      .then((success: Nivel) => {
-        this.notificationService.success('Registro salvo.', 'Sucesso');
-      });
+    const result = await this.nivelService.save(nivel);
+
+    if (result instanceof HttpErrorResponse)
+      return;
+
+    await this.notificationService.success('Registro gravado com sucesso.');
+
+    this.redirectToNivelList();
+  }
+
+  redirectToNivelList(): void
+  {
+    this.router.navigate(['..'], { relativeTo: this.activatedRoute });
   }
 
   private buildForm(): void
@@ -50,6 +60,11 @@ export class NivelFormComponent
       return;
 
     const nivel = await this.nivelService.read(nivelId);
+
+    if (nivel instanceof HttpErrorResponse) {
+      this.redirectToNivelList();
+      return;
+    }
 
     this.form.patchValue({
       id: nivel.id,

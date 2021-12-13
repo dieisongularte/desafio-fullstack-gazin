f85c3326b257629c91f6c2227f6d2546e2db67ce
MisugiHaruo <misugiharuo@gmail.com>
2021-12-13 02:28:38 -0300

    Improve nivel list component

--- app/src/app/views/nivel/list/nivel-list.component.ts
+++ app/src/app/views/nivel/list/nivel-list.component.ts
@@ -3,6 +3,8 @@ import { Nivel } from './../../../entities/nivel';
 import { Component } from '@angular/core';
 import { NivelService } from 'src/app/services/nivel.service';
 import { TableColumn } from 'src/app/components/list/table-column';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'app-nivel-list',
@@ -18,6 +20,7 @@ export class NivelListComponent
   constructor(
     private nivelService: NivelService,
     private activatedRoute: ActivatedRoute,
+    private notificationService: NotificationService,
     private router: Router
   )
   {
@@ -30,6 +33,23 @@ export class NivelListComponent
     this.router.navigate([nivelId], { relativeTo: this.activatedRoute });
   }
 
+  async deleteNivel(nivelId: number): Promise<void>
+  {
+    const doesUserAgreed = await this.requestUserAgreement();
+
+    if (!doesUserAgreed)
+      return;
+
+    const result = await this.nivelService.delete(String(nivelId));
+
+    if (result instanceof HttpErrorResponse)
+      return;
+
+    await this.notificationService.success('Excluído com sucesso.');
+
+    this.loadNiveis();
+  }
+
   private loadTableColumns(): void
   {
     this.columns = [
@@ -40,5 +60,17 @@ export class NivelListComponent
   private async loadNiveis(): Promise<void>
   {
     this.niveis = await this.nivelService.readAll();
+  }
+
+  private async requestUserAgreement(): Promise<boolean>
+  {
+    const { isConfirmed } = await this.notificationService.question(
+      'Deseja realmente, excluir este nível?',
+      'Excluir Nível',
+      'Sim',
+      'Não'
+    );
+
+    return isConfirmed;
   }
 }
